e503af452c8e44beaadb96f2333a25716f3338c7
Ronald Suplina <tyger995@gmail.com>
2023-08-04 23:49:52 +0200

    Add Docker Containers Locally

--- backend/app/settings/config.py
+++ backend/app/settings/config.py
@@ -1,5 +1,12 @@
+import os
+run_mode = os.environ.get('RUN_MODE', 'local')
+if run_mode == 'local':
+    DB_URL = 'mysql+pymysql://root:123@localhost:3306/f1db'
+else:
+    DB_URL = 'mysql+pymysql://root:123@mysql:3306/f1db'
+
 f1_base_url  = 'https://ergast.com/api/f1/'
 f1_base_url_plus_drivers_2023  = 'https://ergast.com/api/f1/2023/drivers.json'
 
 frontend = 'http://localhost:5173'
-DB_URL = 'mysql+pymysql://root:123@localhost:3306/f1db'
+
